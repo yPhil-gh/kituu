547c1f79b0a3d81de078e5669c89f19b68ab4069
px <px@mint>
2012-03-07 00:33:43 +0000

    Coherent installer

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -6,14 +6,14 @@ lispdir=~/.emacs.d/lisp
 scriptsdir=~/scripts
 sep="\n################# "
 
-echo -e "Kituu!"
+echo -e $sep"Kituu! #################"
 
 if [ ! -d $kituudir ]
 then
-    echo -e $sep"No existing $kituudir, cloning..."
+    echo -e $sep"No existing $kituudir, so"
     cd && git clone git@github.com:xaccrocheur/kituu.git
 else
-    echo -e $sep"Found $kituudir, fetching..."
+    echo -e $sep"Found $kituudir, so"
     cd $kituudir && git fetch && git reset --hard origin/master
 fi
 
@@ -33,28 +33,28 @@ done
 if [ ! -e $scriptsdir/git-completion.bash ]
   then
     echo -e $sep"Installing Git completion..."
-    curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > ~/scripts/git-completion.bash
+    curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > $scriptsdir/git-completion.bash
 fi
 
 if [ ! -e $lispdir/tabbar/ ]
   then
     echo -e $sep"Installing Tabbar in $lispdir/tabbar/"
     git clone https://github.com/dholm/tabbar.git
-    rm -rf tabbar/.git/* && rm -rfv tabbar/.git/ && echo -e $sep"...Done."
+    rm -rf $lispdir/tabbar/.git/* && rm -rfv $lispdir/tabbar/.git/ && echo -e $sep"...Done."
 fi
 
 if [ ! -e $scriptsdir/offlineimap/ ]
   then
     echo -e $sep"Installing offlineimap in $scriptsdir/offlineimap/"
     git clone https://github.com/spaetz/offlineimap.git
-    ln -sv offlineimap/offlineimap.el $scriptsdir && echo -e $sep"...Done."
+    ln -sv $scriptsdir/offlineimap/offlineimap.el $scriptsdir && echo -e $sep"...Done."
 fi
 
 if [ ! -e $lispdir/offlineimap.el ]
   then
     echo -e $sep"Installing offlineimap.el in $lispdir/"
     git clone http://git.naquadah.org/git/offlineimap-el.git
-    cp -v offlineimap-el/offlineimap.el . && rm -rf offlineimap-el/* && rm -rfv offlineimap-el/ && echo -e $sep"...Done."
+    cp -v $lispdir/offlineimap-el/offlineimap.el . && rm -rf $lispdir/offlineimap-el/* && rm -rfv $lispdir/offlineimap-el/ && echo -e $sep"...Done."
 fi
 
 
