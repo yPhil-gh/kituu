fa3708f4deddf69528b4e4ef6732498466c897d7
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-03-11 19:18:04 +0000

    Removed offlineimap.*.el. I just want to know when the process is finished. Duh.

---
+++ .emacs.d/lisp/olimap.el
@@ -0,0 +1,44 @@
+;; olimap.el
+(defun olimap-check (ol-output-line)
+  (if (string-match "terminate" ol-output-line)
+      (message "Sync finished on %s" (format-time-string "%Y-%m-%d at %H:%M:%S")))
+  ;; (when
+  ;;     (string-match "copyingmessage" ol-output-line)
+  ;;   (el-get-notify "New Mail!"
+  ;; 		   (format "In (%s) on account (%s)"
+  ;; 			   (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1))
+  ;; 			   (substring ol-output-line  (+ (search "[acc" ol-output-line) 6) (search "]" ol-output-line :from-end t)))))
+  )
+
+(add-hook 'comint-output-filter-functions
+          'olimap-check)
+
+(defconst *main* "~/scripts/offlineimap.py")
+(defconst *argx* "-umachineui")
+
+(defun olimap-run ()
+  "Run an inferior offlineimap process,\
+ input and output via buffer *olineimap*."
+  (interactive)
+  (apply 'make-comint "olineimap" *main* nil
+         (list *argx*)))
+
+(defun olimap-kill ()
+  (interactive)
+  (with-current-buffer "*olineimap*"
+    (comint-kill-subjob)
+    (kill-buffer "*olinemap*")))
+
+(defun olimap-see ()
+  (interactive)
+  (delete-other-windows)
+  (switch-to-buffer-other-window "*olineimap*")
+  (erase-buffer)
+  (other-window -1))
+
+;; (setq ol-output-line "msg:copyingmessage:Folder [Gmail]/Sent Mail [acc: perso]446%0AGmail%0A%5BGmail%5D%2FSent+Mail%0AMaildir%5B%5BGmail%5D.Sent+Mail%5D")
+;; (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1))
+;; (substring ol-output-line  (+ (search "[acc" ol-output-line) 6) (search "]" ol-output-line :from-end t))
+
+(message "olimap loaded")
+(provide 'olimap)
