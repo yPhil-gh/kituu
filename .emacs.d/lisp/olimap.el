8f7ca6994e0572fa33a3a45de64a24eca7c3d15f
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-03-12 01:41:20 +0000

    GPL Notice

--- .emacs.d/lisp/olimap.el
+++ .emacs.d/lisp/olimap.el
@@ -1,26 +1,35 @@
-;; (if (require 'el-get nil 'noerror)
-;;     (progn (string-match "\\[acc:.*\\]"
-;; 			 "msg:copyingmessage:Folder INBOX [acc: adamwebzoul]87%0AIMAP%0AINBOX%0AMaildir%5BINBOX%5D")
-;; 	   (setq mysweetvar (match-string 0 "msg:copyingmessage:Folder INBOX [acc: adamwebzoul]87%0AIMAP%0AINBOX%0AMaildir%5BINBOX%5D"))))
-;; (match-string NUM &optional STRING)
+;;; olimap.el --- Control & monitor Offlineimap
 
-;; ;; olimap.el
-;; (defun olimap-check (ol-output-line)
-;;   (if (string-match "terminate" ol-output-line)
-;;       (progn
-;; 	(message "Sync finished on %s" (format-time-string "%Y-%m-%d at %H:%M:%S"))
-;; 	(gnus-demon-scan-news))
-;;     (when
-;; 	(string-match "copyingmessage" ol-output-line)
-;;       (if (require 'el-get nil 'noerror)
-;; 	  (progn
-;; 	    (el-get-notify "New Mail!"
-;; 			   (format "Mail received at %s on %s (%s)"
-;; 				   (format-time-string "%H:%M:%S")
-;; 				   (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1)) (progn (string-match "\\[acc:.*\\]" ol-output-line) (setq mysweetvar (match-string 0 ol-output-line)))))
-;; 	    (message "test [ %s ]" (substring ol-output-line  (+ (search "[acc" ol-output-line) 6) (search "]" ol-output-line :from-end t))))
-;; 	(message "Mail received at %s on %s" (format-time-string "%H:%M:%S") (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1))))
+;; Copyright (C) 2011 Philippe M. Coatmeur
 
+;; Author: Philippe M. Coatmeur <philippe.coatmeur@gmail.com>
+;; Created: 12 Mars 2012
+;; Version: 1.0
+;; Keywords: gnus imap mail message offlineimap
+;; X-URL: https://github.com/xaccrocheur/kituu/blob/master/.emacs.d/lisp/olimap.el
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;; The idea is to know when offlineimap has finished to do his
+;; business to do things, not before. And while we're at it, try to
+;; figure out what he's doing. And no, no fancy color overlays, thank
+;; you very much.
+
+;; The path to offlineimap
+(defconst *main* "~/scripts/offlineimap.py")
 
 (defun olimap-check (ol-output-line)
   (if (string-match "terminate" ol-output-line)
@@ -40,46 +49,35 @@
       (if
 	  (require 'el-get nil 'noerror)
 	  (el-get-notify "New Mail! Yiii!" (format "Mail received at %s on %s %s" time-received mailbox-name account-name))
-	(message "Mail received at %s on %s %s" time-received mailbox-name account-name))
+	(message "Mail received at %s on %s %s" time-received mailbox-name account-name)))))
 
-
-      ;; (message "Mail received at %s on %s" (format-time-string "%H:%M:%S") (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1)))
-      ;; (el-get-notify "New Mail!"
-      ;; 		     (format "In (%s) on account (%s)"
-      ;; 			     (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1))
-      ;; 			     (substring ol-output-line  (+ (search "[acc" ol-output-line) 6) (search "]" ol-output-line :from-end t))))
-      )))
+(defconst *argx* "-umachineui")
 
 (add-hook 'comint-output-filter-functions
           'olimap-check)
 
-(defconst *main* "~/scripts/offlineimap.py")
-(defconst *argx* "-umachineui")
-
 (defun olimap-run ()
   "Run an inferior offlineimap process,\
- input and output via buffer *olineimap*."
+ input and output via buffer *olimap*."
   (interactive)
   (message "Sync started on %s" (format-time-string "%Y-%m-%d at %H:%M:%S"))
-  (apply 'make-comint "olineimap" *main* nil
+  (apply 'make-comint "ofimap" *main* nil
          (list *argx*)))
 
 (defun olimap-kill ()
+  "cleanly kill OL"
   (interactive)
-  (with-current-buffer "*olineimap*"
+  (with-current-buffer "*olimap*"
     (comint-kill-subjob)
-    (kill-buffer "*olinemap*")))
+    (kill-buffer "*olimap*")))
 
 (defun olimap-see ()
+  "Switch to OL output buffer"
   (interactive)
   (delete-other-windows)
-  (switch-to-buffer-other-window "*olineimap*")
+  (switch-to-buffer-other-window "*olimap*")
   (erase-buffer)
   (other-window -1))
-
-;; (setq ol-output-line "msg:copyingmessage:Folder INBOX [acc: adamweb]87%0AIMAP%0AINBOX%0AMaildir%5BINBOX%5D")
-;; (substring ol-output-line ( + (search ":Folder " ol-output-line) 8) (- (search "[acc" ol-output-line) 1))
-;; (substring ol-output-line  (+ (search "[acc" ol-output-line) 6) (search "]" ol-output-line :from-end t))
 
 (message "%s loaded" (buffer-file-name))
 (provide 'olimap)
