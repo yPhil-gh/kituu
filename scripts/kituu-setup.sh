620b04f8ec3a3a3d3417dc0dcec5b5c6f32953e7
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-03-16 04:44:21 +0000

    New setup

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -5,46 +5,47 @@ kituudir=~/.kituu
 lispdir=~/.emacs.d/lisp
 scriptsdir=~/scripts
 sep="\n################# "
-gitcommand=$(git fetch && git reset --hard origin/master)
 
 echo -e $sep"Kituu! #################"
 
 if [ ! -d $kituudir ]
 then
-    echo -e $sep"No existing $kituudir, so" && git clone git@github.com:xaccrocheur/kituu.git
+echo -e $sep"No existing $kituudir, so"
+    cd && git clone git@github.com:xaccrocheur/kituu.git
 else
-    echo -e $sep"Found $kituudir, so" && $gitcommand
+echo -e $sep"Found $kituudir, so"
+    cd $kituudir && git fetch && git reset --hard origin/master
 fi
 
 for i in * ; do
-    if [[ "${i}" != ".git" &&  "${i}" != "README.org" && "${i}" != "." && "${i}" != ".." && ! -L ~/$i ]] ; then
-	if [[ -e ~/$i ]] ; then
-	    mv -v ~/$i ~/$i.orig
-	    ln -sv $kituudir/$i ~/
-	else
-	    ln -sv $kituudir/$i ~/
-	fi
-    else
-	[[ "${i}" != ".git" ]] && [[ "${i}" != "README.org" ]] && echo -e " \033[1m$i\033[0m \t > \t is already a symlink"
+if [[ "${i}" != ".git" && "${i}" != "README.org" && "${i}" != "." && "${i}" != ".." && ! -L ~/$i ]] ; then
+if [[ -e ~/$i ]] ; then
+mv -v ~/$i ~/$i.orig
+ln -sv $kituudir/$i ~/
+else
+ln -sv $kituudir/$i ~/
+fi
+else
+[[ "${i}" != ".git" ]] && [[ "${i}" != "README.org" ]] && echo -e " \033[1m$i\033[0m \t > \t is already a symlink"
     fi
 done
 
-if [ ! -e $scriptsdir/git-completion.bash ] ; then
-    echo -e $sep"Installing Git completion..."
+if [ ! -e $scriptsdir/git-completion.bash ]
+  then
+echo -e $sep"Installing Git completion..."
     curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > $scriptsdir/git-completion.bash
 fi
 
-if [ ! -e $lispdir/tabbar/ ] ; then
-    echo -e $sep"Installing Tabbar in $lispdir/tabbar/"
-    cd $lispdir &&  git clone https://github.com/dholm/tabbar.git && echo -e $sep"...Done."
-else
-    cd $lispdir/tabbar/ && $gitcommand
+if [ ! -e $lispdir/tabbar/ ]
+  then
+echo -e $sep"Installing Tabbar in $lispdir/tabbar/"
+    cd $lispdir && git clone https://github.com/dholm/tabbar.git
+    rm -rf tabbar/.git/* && rm -rfv $lispdir/tabbar/.git/ && echo -e $sep"...Done."
 fi
 
-if [ ! -e $scriptsdir/offlineimap/ ] ; then
-    echo -e $sep"Installing offlineimap in $scriptsdir/offlineimap/"
+if [ ! -e $scriptsdir/offlineimap/ ]
+  then
+echo -e $sep"Installing offlineimap in $scriptsdir/offlineimap/"
     cd $scriptsdir && git clone https://github.com/spaetz/offlineimap.git
     ln -sv offlineimap/offlineimap.py . && echo -e $sep"...Done."
-else
-    cd $scriptsdir/offlineimap/ && $gitcommand
 fi
