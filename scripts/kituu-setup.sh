82af84a2fcb930401be062d70f0ea4d33ed21b95
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-05-23 05:17:49 +0000

    New distro detect for binary installs

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -3,18 +3,18 @@
 shopt -s dotglob
 kituudir=~/.kituu
 lispdir=~/.emacs.d/lisp
-scriptsdir=~/scripts
+scriptdir=~/scripts
 sep="\n################# "
 
 echo -e $sep"Kituu! #################"
 
-# if [ ! -d $kituudir ] ; then
-#     echo -e $sep"No existing $kituudir, so"
-#     cd && git clone git@github.com:xaccrocheur/kituu.git
-# else
-#     echo -e $sep"Found $kituudir, so"
-#     cd $kituudir && git pull
-# fi
+if [ ! -d $kituudir ] ; then
+    echo -e $sep"No existing $kituudir, so"
+    cd && git clone git@github.com:xaccrocheur/kituu.git
+else
+    echo -e $sep"Found $kituudir, so"
+    cd $kituudir && git pull
+fi
 
 for i in * ; do
     if [[  ! -h ~/$i && $i != *#* && $i != *~* && $i != *git* && $i != "README.org" && $i != "." && "${i}" != ".." ]] ; then
@@ -32,18 +32,39 @@ if $mandriva ; then
     sudo urpmi curl wget bzr git perl-doc sox bbdb htop
 fi
 
-# if [ ! -e $scriptsdir/git-completion.bash ] ; then
-#     echo -e $sep"Git completion ($scriptsdir/git-completion.bash)"
-#     curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > $scriptsdir/git-completion.bash
-# fi
+if [ ! -e $scriptsdir/git-completion.bash ] ; then
+    echo -e $sep"Git completion ($scriptsdir/git-completion.bash)"
+    curl -L https://github.com/git/git/raw/master/contrib/completion/git-completion.bash > $scriptsdir/git-completion.bash
+fi
 
-# if [ ! -e $scriptsdir/leecher/leecher.pl ] ; then
-#     echo -e $sep"leecher.pl ($scriptsdir/leecher.pl)"
-#     cd $scriptsdir && git clone git@github.com:xaccrocheur/leecher.pl.git
-#     ln -sv $scriptsdir/leecher/leecher.pl $scriptsdir/
-# else
-#     cd $scriptsdir/leecher/ && git pull
-# fi
+if [ ! -e $scriptsdir/leecher/leecher.pl ] ; then
+    echo -e $sep"leecher.pl ($scriptsdir/leecher.pl)"
+    cd $scriptsdir && git clone git@github.com:xaccrocheur/leecher.pl.git
+    ln -sv $scriptsdir/leecher/leecher.pl $scriptsdir/
+else
+    cd $scriptsdir/leecher/ && git pull
+fi
+
+declare -A lisp
+lisp[tabbar]="git clone https://github.com/dholm/tabbar.git"
+lisp[tabbar-ruler]="git clone https://github.com/mlf176f2/tabbar-ruler.el.git"
+lisp[undo-tree]="git clone http://www.dr-qubit.org/git/undo-tree.git"
+lisp[nxhtml]="bzr branch lp:nxhtml"
+lisp[zob]="bzr branch lp:nxhtml"
+
+for project in "${!lisp[@]}"
+do
+    vcsystem=${lisp[$project]:0:3}
+    echo -e $sep"$project ($lispdir/$project/)"
+    if [ ! -e $lispdir/$project/ ] ; then
+	read -e -p "Install $project in ($lispdir/$project/)? [Y/n] " yn
+	if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
+	    echo "cd $lispdir && ${lisp[$project]}"
+	fi
+    else
+	echo "cd $lispdir/$project/ && $vcsystem pull"
+    fi
+done
 
 # if [ ! -e $lispdir/nxhtml/ ] ; then
 #     echo -e $sep"nXhtml ($lispdir/nxhtml/)"
