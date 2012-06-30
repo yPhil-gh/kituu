dfa6ab166834e866e756240830fba6865afdfb56
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-06-30 10:40:35 +0000

    curses dev libs

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -35,7 +35,7 @@ done
 
 type -P drakconf &>/dev/null || { mandriva=false >&2; }
 
-packages="zsh curl gcc autoconf automake texinfo libtool libncurses libgtk2.0-dev libxpm-dev libjpeg62-dev libtiff-dev libgif-dev  emacs zile wget bzr git perl-doc sox bbdb htop xfce4 bc thunderbird gimp inkscape wl"
+packages="zsh curl gcc autoconf automake texinfo libtool libncurses5-dev libgtk2.0-dev libxpm-dev libjpeg62-dev libtiff-dev libgif-dev  emacs zile wget bzr git perl-doc sox bbdb htop xfce4 bc thunderbird gimp inkscape wl"
 
 read -e -p "Install various binary packages ($packages)? [Y/n] " yn
 if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
