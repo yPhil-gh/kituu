4a9ebca83224f3c0ceaba5f71a4d10bafd2da5f8
Philippe Coatmeur <xaccrocheur@gmail.com>
2012-06-30 10:23:24 +0000

    gtk libs

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -35,7 +35,7 @@ done
 
 type -P drakconf &>/dev/null || { mandriva=false >&2; }
 
-packages="zsh curl gcc autoconf automake texinfo libtool libgtk2.0.0 emacs zile wget bzr git perl-doc sox bbdb htop xfce4 bc thunderbird gimp inkscape wl"
+packages="zsh curl gcc autoconf automake texinfo libtool libgtk-3-common emacs zile wget bzr git perl-doc sox bbdb htop xfce4 bc thunderbird gimp inkscape wl"
 
 read -e -p "Install various binary packages ($packages)? [Y/n] " yn
 if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
