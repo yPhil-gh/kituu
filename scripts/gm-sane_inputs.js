2c6f0c7435e2faf3e21bb7de7cd15856261e612f
Phil CM <xaccrocheur@gmail.com>
2012-08-07 14:20:58 +0000

    GreaseMonkey script 'Sane Inputs'

---
+++ scripts/gm-sane_inputs.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name           Sane Inputs
+// @author         pX
+// @description    Style Corrector (by pX) reset the input text color for dark env
+// @namespace      userscripts.org/scripts/show/36850
+// @include        *
+// @exclude        http://*.stackoverflow.com/*
+// ==/UserScript==
+
+
+
+// GM_addStyle("input { background-color: white !important; }");
+// GM_addStyle("input { background: none; background-color: black !important; }");
+// GM_addStyle("input { color: red !important; }");
+// GM_addStyle("input { padding: 2px; border: 1px solid; border-color: black; -moz-border-radius: 7px !important; }");
+// GM_addStyle("textarea { background-color: white !important; }");
+GM_addStyle("input[type=text] { background-color: black; color: green; !important; }");
+// GM_addStyle("textarea { padding: 2px; border: 1px solid; border-color: black; -moz-border-radius: 7px !important; }");
+// GM_addStyle("option { background-color: white !important; }");
+// GM_addStyle("option { color: black; font-family: ; font-size:  !important; }");
+// GM_addStyle("option { padding: 0px; border: 0px solid; border-color: black; -moz-border-radius: 4px !important; }");
+// GM_addStyle("select { background-color: white !important; }");
+// GM_addStyle("select { color: black; font-family: ; font-size:  !important; }");
+// GM_addStyle("select { padding: 3px; border: 1px solid; border-color: black; -moz-border-radius: 7px !important; }");
+// GM_addStyle("button { background-color: white !important; }");
+// GM_addStyle("button { color: black; font-family: ; font-size:  !important; }");
+// GM_addStyle("button { padding: 3px; border: 1px solid; border-color: black; -moz-border-radius: 7px !important; }");
