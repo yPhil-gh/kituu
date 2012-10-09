cd449a22f3eede0ff771142b86a8f7c952e40a8d
xaccrocheur <xaccrocheur@gmail.com>
2012-10-09 10:23:11 +0000

    Package cleanup

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -13,16 +13,16 @@ if ($rw); then vc_prefix="git@github.com:" && message="RW mode ON" && git config
 
 # My binary packages
 declare -A pack
-pack[base_sys]="zsh apt-file curl wget htop bc locate opensh-server"
-pack[dev_tools]="gcc autoconf automake texinfo libtool bzr git cvs subversion"
+pack[base_sys]="zsh apt-file curl wget htop bc locate openssh-server"
+pack[dev_tools]="build-essential texinfo libtool bzr git cvs subversion"
 pack[base_utils]="emacs vim unison filelight gparted"
-pack[view&players]="sox okular eog imagemagick smplayer gstreamer0.10-plugins clementine"
+pack[view&players]="sox okular"
 pack[image_tools]="gimp inkscape blender"
 pack[music_prod]="qtractor invada-studio-plugins-lv2 ir.lv2 lv2fil mda-lv2 lv2vocoder so-synth-lv2 swh-lv2 vmpk qmidinet calf-plugins nekobee"
 pack[glamp]="apache2 mysql-server phpmyadmin"
 pack[games]="extremetuxracer supertuxkart torcs nexuiz"
-pack[xubuntu]="xubuntu"
-pack[xscreensaver]="xscreensaver-gl xscreensaver-gl-extra xscreensaver-data-extra xscreensaver-data"
+# pack[xscreensaver]="xscreensaver-gl xscreensaver-gl-extra xscreensaver-data-extra xscreensaver-data"
+# pack[xubuntu]="xubuntu"
 # pack[dev_env]="perl-doc"
 # pack[dev_libs]="libncurses5-dev libgnutls-dev librsvg2-dev libxpm-dev libjpeg62-dev libtiff-dev libgif-dev libqt4-dev libgtk-3-dev"
 # pack[emacs_friends]="bbdb mailutils w3m-el"
