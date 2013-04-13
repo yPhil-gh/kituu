883a747c60f1470722d7e7fb20e28f5694305d58
xaccrocheur <xaccrocheur@gmail.com>
2013-04-13 19:30:38 +0000

    BASH-compatible merciful checkup

--- .kituu-commands.sh
+++ .kituu-commands.sh
@@ -70,7 +70,9 @@ px-vnc () {
 
 px-dirsizes () { for dir in $1* ; do if [ -d $dir ] ; then du -hsL $dir ; fi ; done }
 
-px-websearch () { firefox "https://duckduckgo.com/?q=$*" }
+px-websearch () {
+    command firefox "https://duckduckgo.com/?q=$*"
+}
 
 # px-find-this-and-do-that () { find . -name $1 -exec $2 '{}' \; }
 
