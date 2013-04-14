fabc712c6b815fc3726443c53207b60ed06c3b97
xaccrocheur <xaccrocheur@gmail.com>
2013-04-14 21:34:22 +0000

    Bash annoyances again

--- .kituu-commands.sh
+++ .kituu-commands.sh
@@ -11,7 +11,10 @@ alias cp="cp -i"
 
 alias grep="grep -rnIs --color"
 
-# if [ ! $HOSTNAME == "N900" ] ; then alias grep="grep --color" ; fi
+if [[ ! $HOSTNAME == "N900" ]] ; then
+    # echo "kk"
+    alias grep="grep --color"
+fi
 
 alias ll="ls -lha"
 alias la="ls -A"
