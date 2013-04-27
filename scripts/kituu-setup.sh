94a665fcf80b5f2eaa8eb86aa278da2215813a07
xaccrocheur <xaccrocheur@gmail.com>
2013-04-27 22:55:22 +0000

    Mandatory sox

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -91,14 +91,15 @@ else
     if [[ ! $(grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | cut -d: -f2,3 | sed '/^\#/d' | sed '/^$/d' | grep cassou) ]] ; then sudo add-apt-repository ppa:cassou/emacs && sudo apt-get update ; else echo -e "Emacs 24 repo \t\tOK" ; fi
 fi
 
+echo -e $sep"Mandatory packages (no questions asked)"
+sudo apt-get install aptitude zsh vim byobu apt-file curl wget htop bc locate openssh-server sshfs bzr git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap xclip sox
+
 # Packages
 if $debian; then
     echo -e $sep"Binary packages"
     read -e -p "#### Install packages? [Y/n] " yn
 
     if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
-    echo -e $sep"Mandatory packages (no questions asked)"
-    sudo apt-get install aptitude zsh vim byobu apt-file curl wget htop bc locate openssh-server sshfs bzr git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap xclip sox
 	for group in "${!pack[@]}" ; do
 	    read -e -p "
 ## Install $group? (${pack[$group]})
