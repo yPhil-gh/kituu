5b4e0da3487fd971cfa0457839be610644cbc42e
xaccrocheur <xaccrocheur@gmail.com>
2013-07-12 14:20:27 +0000

    px-pull-and-build.sh

---
+++ scripts/px-pull-and-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull 1>&1 | grep "Already up-to-date."
+if [ ! $? -eq 0 ]; then
+    read -e -p "## Branch moved, build and install $1? [Y/n] " yn
+    if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
+        ./waf configure && ./waf && sudo ./waf uninstall && sudo ./waf install
+    fi
+fi
