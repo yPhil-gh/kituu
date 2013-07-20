da1da57b87868180a7d7bd5d21c00c694a00e195
xaccrocheur <xaccrocheur@gmail.com>
2013-07-20 21:49:44 +0000

    New update script

--- scripts/px-update-repos-build-install.sh
+++ scripts/px-update-repos-build-install.sh
@@ -3,3 +3,5 @@ git clone https://code.google.com/p/amsynth/
 svn co http://svn.drobilla.net/lad/trunk drobilla-lad
 svn co http://svn.code.sf.net/p/drumkv1/code/trunk drumkv1-svn
 svn checkout svn://svn.code.sf.net/p/arpage/code/trunk arpage
+
+
