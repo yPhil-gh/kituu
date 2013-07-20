5663170ae66d9b6632af646ee98b4bce7af54a27
xaccrocheur <xaccrocheur@gmail.com>
2013-07-20 21:47:23 +0000

    New update script

---
+++ scripts/px-update-repos-build-install.sh
@@ -0,0 +1,5 @@
+git clone git://git.code.sf.net/p/triceratops/code triceratops-code
+git clone https://code.google.com/p/amsynth/
+svn co http://svn.drobilla.net/lad/trunk drobilla-lad
+svn co http://svn.code.sf.net/p/drumkv1/code/trunk drumkv1-svn
+svn checkout svn://svn.code.sf.net/p/arpage/code/trunk arpage
