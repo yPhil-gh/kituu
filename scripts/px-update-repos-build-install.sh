425c5d8c328c1c6dd7d2e624ca57533bc1dc1d7f
xaccrocheur <xaccrocheur@gmail.com>
2013-07-21 14:01:13 +0000

    New update script: RC1

--- scripts/px-update-repos-build-install.sh
+++ scripts/px-update-repos-build-install.sh
@@ -2,15 +2,17 @@
 
 srcdir=~/src
 
+[[ $1 == "-f" ]] && force=true || force=false
+
 declare -A pack
-pack[02-triceratops]="git clone git://git.code.sf.net/p/triceratops/code"
-pack[02-amsynth]="git clone https://code.google.com/p/amsynth"
+# pack[02-triceratops]="git clone git://git.code.sf.net/p/triceratops/code"
+# pack[02-amsynth]="git clone https://code.google.com/p/amsynth"
 # pack[sord]="svn co http://svn.drobilla.net/sord/trunk"
 # pack[lilv]="svn co http://svn.drobilla.net/lad/trunk/lilv"
 pack[02-drumkv1]="svn co http://svn.code.sf.net/p/drumkv1/code/trunk"
 pack[03-ardour]="git clone git://git.ardour.org/ardour/ardour.git"
-pack[01-drobilla-lad]="svn co http://svn.drobilla.net/lad/trunk"
-pack[00-lv2]="svn checkout http://lv2plug.in/repo/trunk"
+# pack[01-drobilla-lad]="svn co http://svn.drobilla.net/lad/trunk"
+# pack[00-lv2]="svn checkout http://lv2plug.in/repo/trunk"
 
 pack_indexes=( ${!pack[@]} )
 # IFS=$'\n'
@@ -41,15 +43,17 @@ init=true
 
 read -e -p "## Install deps? [Y/n] " yn
 if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
-    sudo apt-get install autoconf libboost-dev libglibmm-2.4-dev libsndfile-dev liblo-dev libxml2-dev uuid-dev libcppunit-dev libfftw3-dev libaubio-dev liblrdf-dev libsamplerate-dev libsratom-dev libsuil-dev libgnomecanvas2-dev libgnomecanvasmm-2.6-dev libcwiid-dev libgtkmm-2.4-dev
+    sudo apt-get install autoconf libboost-dev libglibmm-2.4-dev libsndfile-dev liblo-dev libxml2-dev uuid-dev libcppunit-dev libfftw3-dev libaubio-dev liblrdf-dev libsamplerate-dev libgnomecanvas2-dev libgnomecanvasmm-2.6-dev libcwiid-dev libgtkmm-2.4-dev
 fi
+
+# libsratom-dev libsuil-dev liblilv-0-0
 
 function build_waf {
 
     if [[ $1 = "ardour" ]] ; then
 	read -e -p "## Build ardour with Windows VST support? [Y/n] " yn
 	if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
-            sudo apt-get wine-dev
+            sudo apt-get install wine-dev
             build_flags="--windows-vst"
         else
             build_flags=""
@@ -73,7 +77,6 @@ function build_make {
 function update_package {
 
     echo -e "\n## $1"
-    pwd
     if [[ $init = true || $force = true ]] ; then
         [[ -f ./waf ]] && build_waf $1 || build_make
     else
@@ -111,7 +114,7 @@ for package in "${pack_sorted[@]}" ; do
     package=${package:3:$name_length}
     # echo $package
 
-    echo "ze pack iz $pname : $package_clone_command ($vcsystem)"
+    # echo "ze pack iz $pname : $package_clone_command ($vcsystem)"
 
     # echo -e $sep"$package ($srcdir/$package/)"
     if [[ ! -d $srcdir/$package ]] ; then
@@ -119,13 +122,7 @@ for package in "${pack_sorted[@]}" ; do
         echo
         read -e -p "## $vcinitcommand $package in ($srcdir/$package/)? [Y/n] " yn
         if [[ $yn == "y" || $yn == "Y" || $yn == "" ]] ; then
-            # cd $srcdir && ${pack[$package]}
-            # echo $package_clone_command
-            # exit 0
-            cd $srcdir
-            # ${pack[$package]}
-            pwd
-            $package_clone_command $package && cd $package
+            cd $srcdir && $package_clone_command $package && cd $package
             update_package $package
         fi
     else
