2b54f680744bab3a7ff8212ffb3c4d9657f904d6
xaccrocheur <xaccrocheur@gmail.com>
2013-07-24 18:30:56 +0000

    px-install-binary-plugins.sh : It's a hack

--- scripts/px-install-binary-plugins.sh
+++ scripts/px-install-binary-plugins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ~/tmp/zz
-pwd
+SOURCE_DIR=~/tmp/
+DEST_DIR=/usr/local/lib/lv2/
 
-# exit 0
+cd $SOURCE_DIR
 
 strpos() {
     str=${1/$2*/}
@@ -20,55 +20,26 @@ http://downloads.sourceforge.net/project/distrho/Ports/Wolpertinger/wolpertinger
 
 for D_URL in $DISTRHO ; do
 
-    echo -e "
-entering $(pwd)"
-
     D_FILE=$(basename $D_URL)
     D_DIR=${D_FILE:0:$(( ${#D_FILE} -3 ))}
 
-    echo "
-## $D_DIR"
-    # strpos $D_DIR "_linux32"
-    # POS=$?
-    # # D_FILE_LENGH=$((${POS} +8))
+    strpos $D_FILE "_linux32"
+    POS=$?
 
-    echo "Name should be ${D_DIR:0:$(( ${POS} ))} given that pos is $POS"
-    # echo ${D_DIR:0:26}
-
-    # COOL_NAME=${D_DIR:0:$(( ${POS} ))}
-
-    wget $D_URL
-    p7zip -d $D_FILE
-    cd $(ls)
     echo -e "
-## ${D_DIR:0:$(( ${POS} ))} ($D_FILE) wow, i'm in $(pwd)"
-    # exit 0
-    # cp -R -v lv2/* /usr/local/lib/lv2/
-    # cp -R -v lv2/* ~/tmp/zzz/
+## ${D_DIR:0:$(( ${POS} ))}"
 
-    cd lv2
+    wget -nv $D_URL
+    p7zip -d $D_FILE
+    cd "$(ls)/lv2"
+
+    # cd lv2
 
     for LV2_PLUGIN_DIR in $(ls) ; do
-        cp -R -v $LV2_PLUGIN_DIR ~/tmp/zzz/
+        cp -R $LV2_PLUGIN_DIR $DEST_DIR
     done
 
-    echo -e "
-leavin $(pwd)"
+    echo -e "## Copied $(pwd) to $DEST_DIR"
     cd ../..
     rm -rf $(ls)
-
-    # # exit 0
-    # echo "Yo, D_URL iz $D_URL"
-    # echo "Yo, D_FILE iz $D_FILE"
-    # echo "Yo, dir iz $D_DIR"
 done
-
-
-
-# http://sourceforge.net/projects/distrho/files/Ports/Arctican-Plugins/arctican-plugins_linux32_20120518.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/dRowAudio-Plugins/drowaudio-plugins_linux32_20120518.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/Juced-plugins/juced-plugins_linux32_20120518-2.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/TAL-Plugins/tal-plugins_linux32_20120518.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/HighLife/highlife_linux32_20120518.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/HybridReverb2/hybridreverb2_linux32_20120518.7z/download
-# http://sourceforge.net/projects/distrho/files/Ports/Wolpertinger/wolpertinger_linux32_20120518-2.7z/download
