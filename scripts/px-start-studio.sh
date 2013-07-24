f6ed68d62554f9532cc61e8a0819665498412527
xaccrocheur <xaccrocheur@gmail.com>
2013-07-24 13:46:00 +0000

    All-new start-studio script

--- scripts/px-start-studio.sh
+++ scripts/px-start-studio.sh
@@ -3,27 +3,30 @@
 #VST_PATH=/usr/lib/vst:/usr/local/lib/vst
 #export VST_PATH
 
-apps="killall qjackctl vmpk qmidinet jackd"
+APPS="
+/usr/lib/qjackctl/qjackctl.real
+vmpk
+qmidinet
+qtractor
+"
+
+kill_all() {
+    for APP in $APPS ; do
+        if [[ $(pidof $APP) ]] ; then
+            echo  "## Killing $APP ($(pidof $APP))"
+            killall $APP
+        fi
+    done
+}
 
 if [ "$1" == "-k" ] ; then
-    killall $apps ardour-3.2 qjackctl.real
+    kill_all
 else
-    killall -w $apps ardour-3.2 qjackctl.real
-    sleep 1
-    qjackctl &
-    sleep 2
-    qmidinet &
-    # sleep 1
-    # ams -N Guitar -l ~/Documents/Music/00-PRESETS/MyGrt00.ams &
-    # sleep 1
-    # ams -N BassSynth -l ~/Documents/Music/00-PRESETS/MySynth000.ams &
+    kill_all
 
-    ardour3 >nul 2>&1 &
-
-    # if [ "$1" == "-d" ] ; then
-    #     /usr/local/bin/qtractor -style -plastique $2 &
-    # else
-    #     qtractor -style -plastique $1 &
-    # fi
-   vmpk &
+    for APP in $APPS ; do
+        $APP&
+        echo "## Starting $APP ($(pidof $APP))"
+        sleep 1
+    done
 fi
