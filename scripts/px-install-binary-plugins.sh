27980722141dd1281c30c4c012370b321faef3e1
xaccrocheur <xaccrocheur@gmail.com>
2013-07-26 15:30:37 +0000

    Now quite standard

--- scripts/px-install-binary-plugins.sh
+++ scripts/px-install-binary-plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PLUGIN_PACKS="http://downloads.sourceforge.net/project/distrho/Ports/HighLife/highlife_linux32_20120518.7z
+https://sites.google.com/site/ccernnaudio/vst-plugins/backup.zip
 http://downloads.sourceforge.net/project/distrho/Ports/Arctican-Plugins/arctican-plugins_linux32_20120518.7z
 http://www.mucoder.net/en/hypercyclic/v0101/download/latest/hypercyclic.1.1.367.linux.zip
 http://downloads.sourceforge.net/project/distrho/Ports/dRowAudio-Plugins/drowaudio-plugins_linux32_20120518.7z
@@ -9,16 +10,9 @@ http://downloads.sourceforge.net/project/distrho/Ports/TAL-Plugins/tal-plugins_l
 http://downloads.sourceforge.net/project/distrho/Ports/HybridReverb2/hybridreverb2_linux32_20120518.7z
 http://downloads.sourceforge.net/project/distrho/Ports/Wolpertinger/wolpertinger_linux32_20120518-2.7z"
 
-[[ ! $(groups $USER | grep "audio") ]] && echo "User $USER is not in the audio group, adding it." && sudo adduser $USER audio && echo "
-#### Your user/settings have been reloaded : Now re-run $0.
+[[ ! $(groups $USER | grep "audio") ]] && echo "### User $USER is not in the audio group, adding it." && sudo adduser $USER audio && echo "
+## Your user/settings have been reloaded : Now re-run $0.
 " && sudo su - $USER
-
-# exit 0
-
-# sudo su - $USER
-
-# http://www.mucoder.net/en/hypercyclic/v0101/download/latest/hypercyclic.1.1.367.linux.zip
-# https://dl.dropbox.com/u/4063301/ThrillseekerXTC_1.0.1.zip
 
 SRC_DIR=~/tmp/PLUGIN_PACKS
 # LV2_DIR=/usr/local/lib/lv2/
@@ -33,49 +27,31 @@ LXVST_DIR=~/tmp/ZVST
 [[ ! -w $LV2_DIR ]] && sudo chown -R .audio $LV2_DIR && sudo chmod -R g+w $LV2_DIR
 [[ ! -w $LXVST_DIR ]] && sudo chown -R .audio $LXVST_DIR && sudo chmod -R g+w $LXVST_DIR
 
-# Now just
-# sudo su - $USER && $0
-
 for D_URL in $PLUGIN_PACKS ; do
     D_FILE=$(basename $D_URL)
-
+    D_URI=${D_URL:0:$( expr ${#D_URL} - ${#D_FILE} )}
     echo -e "
-## ${D_FILE}"
+## Downloading ${D_FILE} (from $D_URI)"
 
-    wget -nv $D_URL && 7z x $D_FILE
+    rm -rf *
+
+    wget -q --secure-protocol=auto $D_URL && echo "## Downloaded $D_FILE in $SRC_DIR" && 7z x $D_FILE  > /dev/null
     PLUGIN_LV2=$(find . -name "*.lv2")
     PLUGIN_VST=$(find . -name "*.so")
 
     if [[ $PLUGIN_LV2 ]] ; then
         PLUGIN_VST=""
         D_DEST_DIR=$LV2_DIR
-        # echo "
-#### kayn LV2! ($PLUGIN_LV2) in $D_DEST_DIR" ;
     else
         if [[ $PLUGIN_VST && ! $PLUGIN_LV2 ]] ; then
         D_DEST_DIR=$LXVST_DIR
-            # echo "
-#### kayn VST! ($PLUGIN_VST) in $D_DEST_DIR" ;
         fi
     fi
 
     ALL_PLUGINS="$PLUGIN_VST $PLUGIN_LV2"
 
-    echo "
-### All plugz : $ALL_PLUGINS
-"
-
     for D_PLUGIN in $ALL_PLUGINS ; do
-        echo "
-### Copying $D_PLUGIN to $D_DEST_DIR"
+        echo "## Copying $D_PLUGIN to $D_DEST_DIR"
         cp -R $D_PLUGIN $D_DEST_DIR
     done
-
-    # for LV2_PLUGIN in $PLUGIN_LV2 ; do
-    #     echo "Copying $LV2_PLUGIN to $LV2_DIR"
-    #     cp -R $LV2_PLUGIN $LV2_DIR
-    # done
-
-    # echo -e "## Copied $LV2_PLUGIN to $LV2_DIR"
-    rm -rf *
 done
