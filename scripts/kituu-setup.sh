c5d974237dae4513269d54251b979e5d1af18970
xaccrocheur <xaccrocheur@gmail.com>
2013-08-12 00:03:14 +0100

    python deps packages

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -32,7 +32,7 @@ pack[emacs24_stable]="emacs24 emacs24-el emacs24-common-non-dfsg aspell-fr"
 pack[emacs24_snapshot]="snapshot-el emacs-snapshot-gtk emacs-snapshot aspell-fr"
 pack[calf_plugins_git_tools]="libtool autoconf libexpat1-dev libfftw3-dev libglib2.0-dev libfluidsynth-dev jackd1 lv2core libglade2-dev gtk2-engines-pixbuf"
 
-BASICS="aptitude python python-pip zsh vim byobu apt-file curl wget htop bc locate openssh-server sshfs bzr git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap xclip sox network-manager-openvpn openjdk-7-jre icedtea-7-plugin p7zip-full git-el"
+BASICS="aptitude python python-pip zsh vim byobu apt-file curl wget htop bc locate openssh-server sshfs bzr git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap xclip sox network-manager-openvpn openjdk-7-jre icedtea-7-plugin p7zip-full magit python-scipy"
 
 # MOZilla addons
 MOZURL="https://addons.mozilla.org/firefox/downloads/latest"
