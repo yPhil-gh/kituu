9f16cbee9c8e3b574e1877368bbf1d2ae40e7233
xaccrocheur <xaccrocheur@gmail.com>
2013-09-07 14:37:18 +0100

    magit now elpa-installed

--- scripts/kituu-setup.sh
+++ scripts/kituu-setup.sh
@@ -34,7 +34,7 @@ pack[emacs24_stable]="emacs24 emacs24-el emacs24-common-non-dfsg aspell-fr"
 pack[emacs24_snapshot]="snapshot-el emacs-snapshot-gtk emacs-snapshot aspell-fr"
 pack[calf_plugins_git_tools]="libtool autoconf libexpat1-dev libfftw3-dev libglib2.0-dev libfluidsynth-dev jackd1 lv2core libglade2-dev gtk2-engines-pixbuf"
 
-BASICS="python zsh vim byobu apt-file curl wget htop bc locate sshfs git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap sox p7zip-full magit links"
+BASICS="python zsh vim byobu apt-file curl wget htop bc locate sshfs git subversion cowsay fortune fortunes-off zenity vinagre x11vnc ccze nmap sox p7zip-full links"
 
 # icedtea-7-plugin
 
