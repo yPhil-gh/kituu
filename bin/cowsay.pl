c781a0c4132834ee7575fd661da39200c07a567f
Philippe Coatmeur <pcoatmeur@sbcmaroc.com>
2015-06-20 16:36:26 +0000

    Cowsay

--- bin/cowsay.pl
+++ bin/cowsay.pl
@@ -3,20 +3,23 @@
 # A simple clone of cowsay for when it's not in your repos
 
 $width = 40;
-$o = "oO";
+$o = "oo";
 
 use Text::Wrap;
 
 if ($#ARGV + 1 == 1) {
     if ($ARGV[0] eq "fortune") {
         $moo = `/usr/games/fortune -a`;
+        $o = "-o";
     } elsif ($ARGV[0] eq "dlfp") {
         $moo = `curl http://sam.linuxfr.org/random.txt 2>/dev/null`;
+        $o = "--";
     } else {
         $moo = $ARGV[0];
     }
 } else {
-    $moo = "I got nothing to moo";
+    $moo = "I got nothing to moo (try fortune or dlfp as arg)";
+    $o = "oO";
 }
 
 $Text::Wrap::columns = $width;
@@ -26,11 +29,8 @@ $Text::Wrap::separator = "|$/";
 chomp($moo);
 
 my $linewidth = $width - 1;
-
 my $top =  "\n " . '_' x $linewidth . "\n";
 my $bottom =  "\n " . '-' x $linewidth . "\n";
-
-
 my $cow ="       \\   ^__^
         \\  ($o)\\_______
            (__)\\       )\\/\\
