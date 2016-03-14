155bd6d39a3ba7625895fa2f083f3647dde09ad1
xaccrocheur <xaccrocheur@gmail.com>
2016-03-14 19:52:08 +0000

    LV2

--- scripts/px-audio-plugins.sh
+++ scripts/px-audio-plugins.sh
@@ -2,13 +2,13 @@
 
 selection=$(zenity --list --multiple --checklist --hide-column=4 --print-column=4 --separator="|" --title="LV2 Plugins" --column=Go --column=Type --column=Name --column=URI < \
     <(for plugin in $(lv2ls); do
-    NAME=$(lv2info "$plugin" | grep 'Name:' | grep -v http | awk '{print $0;exit}')
-    CLASS=$(lv2info "$plugin" | grep 'Class:' | grep -v http | awk '{print $2;exit}')
-    echo "Go"
-    echo ${CLASS}
-    echo ${NAME#*:}
-    echo ${plugin}
-done))
+        NAME=$(lv2info "$plugin" | grep 'Name:' | grep -v http | awk '{print $0;exit}')
+        CLASS=$(lv2info "$plugin" | grep 'Class:' | grep -v http | awk '{print $2;exit}')
+        echo "Go"
+        echo ${CLASS}
+        echo ${NAME#*:}
+        echo ${plugin}
+        done))
 
 IFS='|' read -ra URI <<< "$selection"
 for i in "${URI[@]}"; do
