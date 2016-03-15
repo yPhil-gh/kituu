692008a814440ebf1c0bded0948aa20f85f6d74e
xaccrocheur <xaccrocheur@gmail.com>
2016-03-15 23:02:04 +0000

    No URI test

--- scripts/px-audio-plugins.sh
+++ scripts/px-audio-plugins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selection=$(zenity --list --multiple --checklist --hide-column=4 --print-column=4 --separator="|" --title="LV2 Plugins" --column=Go --column=Type --column=Name --column=URI < \
+selection=$(zenity --list --multiple --checklist --hide-column=4 --print-column=4 --separator="|" --title="LV2 Plugins" --column=Go --column=Class --column=Name --column=URI < \
     <(for uri in $(lv2ls); do
         PLUGIN=$(lv2info $uri | head -n +4 | tail -n +3 | paste -sd "|" - | sed 's/Name://g;s/Class://g')
         echo "Go"
