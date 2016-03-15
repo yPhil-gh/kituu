db6e9918d1914786bf4f404bd80b02162365e6be
xaccrocheur <xaccrocheur@gmail.com>
2016-03-15 23:09:56 +0000

    Semantics+12 lines

--- scripts/px-audio-plugins.sh
+++ scripts/px-audio-plugins.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 selection=$(zenity --list --multiple --checklist --hide-column=4 --print-column=4 --separator="|" --title="LV2 Plugins" --column=Go --column=Class --column=Name --column=URI < \
-    <(for uri in $(lv2ls); do
-        PLUGIN=$(lv2info $uri | head -n +4 | tail -n +3 | paste -sd "|" - | sed 's/Name://g;s/Class://g')
-        echo "Go"
-        echo ${PLUGIN#*|}
-        echo ${PLUGIN%|*}
-        echo ${uri}
+    <(for URI in $(lv2ls); do
+        PLUGIN=$(lv2info $URI | head -n +4 | tail -n +3 | paste -sd "|" - | sed 's/Name://g;s/Class://g' | xargs)
+        echo -e "Go \n ${PLUGIN#*|} \n ${PLUGIN%|*} \n ${URI}"
         done))
 
-IFS='|' read -ra URI <<< "$selection"
-for i in "${URI[@]}"; do
+IFS='|' read -ra URIs <<< "$selection"
+for i in "${URIs[@]}"; do
     jalv.gtkmm --name=${i##*/} ${i} &
 done
