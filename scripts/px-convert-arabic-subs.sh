4aeedcedb4c2baeb1a38c02c546ceb4acaf1d2c7
xaccrocheur <xaccrocheur@gmail.com>
2016-04-05 02:06:57 +0100

    Small refactors

--- scripts/px-convert-arabic-subs.sh
+++ scripts/px-convert-arabic-subs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-then
-	echo "USAGE: converts_sub.sh <filename.sub>"
-	exit
-fi
+[[ $# -eq 0 ]] && echo "USAGE: $0 subtitle_file" ; exit
 
 mv "$1" "/tmp/$1.old.$$"
 iconv -f windows-1256 -t utf8 "/tmp/$1.old.$$" -o "$1" && echo "Done"
