b6ff0a5a0aeb0c8253de797f7b1e8efed768fdab
xaccrocheur <xaccrocheur@gmail.com>
2016-05-26 16:27:39 +0100

    px-screencast script

--- scripts/px-screencast.sh
+++ scripts/px-screencast.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+trap killgroup SIGINT
+
+killgroup(){
+  echo killing...
+  kill 0
+}
+
 FILE="${1:-}"
 
 if [[ -e "$FILE.mp4" ]] ; then
-    echo "file exists"
-    exit
+    read -e -p "## File ($FILE) exists, delete? [y/N] " YN
+    if [[ $YN == "n" || $YN == "N" || $YN == "" ]] ; then
+        exit
+    else
+        rm -rf "$FILE"*
+    fi
 fi
 
+key-mon --visible_click -l --backgroundless -t modern &
+screenkey &
 ffmpeg -f x11grab  -s 1920x1200 -i :0.0 -vcodec libx264 "$FILE.mp4" &
-jack_capture -c 2 -p system:capture_1 -p Qtractor:Master/out_1 -p Qtractor:Master/out_2 "$FILE.wav"
+jack_capture -c 2 -p system:capture_1 -p Qtractor:Master/out_1 -p Qtractor:Master/out_2 "$FILE.wav" &
 wait
