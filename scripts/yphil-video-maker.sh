e9d6a11d2fa2fffdfd5a8d4ebcd6cf0d7dd7021f
windowsrefund <home>
2019-01-05 04:54:31 -0500

    bump audio quality

--- scripts/yphil-video-maker.sh
+++ scripts/yphil-video-maker.sh
@@ -77,7 +77,7 @@ fi
 convert -resize 1280x720 "${IMAGE}" -background none -gravity center -extent 1280x720 $IMAGE_TMP
 
 $FFMPEG -y -i "$WAV" -loop 1 -i $IMAGE_TMP -filter_complex \
-  "[0:a] showwaves=s=1280x310:mode=${STYLE}:r=25,colorkey=0x000000:0.1:0.5[wave];[1:v][wave] overlay=y=H-h:eval=init[canvas];[canvas]drawtext=fontfile='Jura-Book.ttf':fontsize=60:text='${ARTIST}':x=30:y=(h-70-text_h*2.5):fontcolor=white:shadowy=2:shadowx=2:shadowcolor=black,drawtext=fontfile='Jura-Book.ttf':fontsize=72:text='${TITLE}':x=20:y=(h-70-text_h):fontcolor=ff6600:shadowy=2:shadowx=2:shadowcolor=black" -shortest -codec:v libx264 -crf 18 -preset fast -codec:a aac -strict -2 -b:a 192k $MP4_TMP
+  "[0:a] showwaves=s=1280x310:mode=${STYLE}:r=25,colorkey=0x000000:0.1:0.5[wave];[1:v][wave] overlay=y=H-h:eval=init[canvas];[canvas]drawtext=fontfile='Jura-Book.ttf':fontsize=60:text='${ARTIST}':x=30:y=(h-70-text_h*2.5):fontcolor=white:shadowy=2:shadowx=2:shadowcolor=black,drawtext=fontfile='Jura-Book.ttf':fontsize=72:text='${TITLE}':x=20:y=(h-70-text_h):fontcolor=ff6600:shadowy=2:shadowx=2:shadowcolor=black" -shortest -codec:v libx264 -crf 18 -preset fast -codec:a aac -strict -2 -b:a 320k $MP4_TMP
 
 if [[ -e end.mp4 ]]; then
     $FFMPEG -y -i $MP4_TMP -i end.mp4 -filter_complex "[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]" -map "[v]" -map "[a]" ${OUTPUT}
